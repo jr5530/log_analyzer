982414998959e4c9ae1f955e1836b0d32f15e693
יהונתן רובין <yr8221@gmail.com>
2026-07-14 11:23:03 +0300

    test delete

--- analyzer.py
+++ analyzer.py
@@ -4,5 +4,4 @@ def check_outsiders_ip(data):
     res = [log[1] for log in data if log[1][:3] != "10." and log[1][:7] != "192.168"]
     return res
 
-print(check_outsiders_ip(reader.read_csv_to_list("network_traffic.log")))
 
