bc235073e0272e998882599e08c934621897afbd
יהונתן רובין <yr8221@gmail.com>
2026-07-14 12:10:46 +0300

    filter_sensitive_ports added

---
+++ config.py
@@ -0,0 +1,7 @@
+
+from reader import read_csv_to_list
+def filter_sensitive_ports(data1):
+    sensitive_ports = {"22", "23", "3389"}
+    res = [log for log in data1 if log[3] in sensitive_ports]
+    return res
+print(filter_sensitive_ports(read_csv_to_list("network_traffic.log")))
