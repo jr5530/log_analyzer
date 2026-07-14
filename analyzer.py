bcd6758139770fa22430af829aebb4dcf9e8eb9d
יהונתן רובין <yr8221@gmail.com>
2026-07-14 14:43:01 +0300

    count_source_ips_fast added

--- analyzer.py
+++ analyzer.py
@@ -11,5 +11,10 @@ def filter_by_size(data):
 def tag_traffic(data):
     res = ["LARGE" if int(log[5]) > 5000 else "NORMAL" for log in data]
     return res
-print(tag_traffic(reader.read_csv_to_list("network_traffic.log")))
+# print(tag_traffic(reader.read_csv_to_list("network_traffic.log")))
 
+from collections import Counter
+
+def count_source_ips_fast(data):
+    return dict(Counter(log[1] for log in data))
+print(count_source_ips_fast(reader.read_csv_to_list("network_traffic.log")))
