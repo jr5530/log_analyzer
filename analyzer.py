ea29a457ded4c159412f4c125b0a89e98014e679
יהונתן רובין <yr8221@gmail.com>
2026-07-14 14:24:12 +0300

    filter by size added

--- analyzer.py
+++ analyzer.py
@@ -4,4 +4,8 @@ def check_outsiders_ip(data):
     res = [log[1] for log in data if log[1][:3] != "10." and log[1][:7] != "192.168"]
     return res
 
+def filter_by_size(data):
+    res = [ log for log in data if int (log[5]) > 5000]
+    return res
+print(filter_by_size(reader.read_csv_to_list("network_traffic.log")))
 
--- config.py
+++ config.py
@@ -4,4 +4,4 @@ def filter_sensitive_ports(data1):
     sensitive_ports = {"22", "23", "3389"}
     res = [log for log in data1 if log[3] in sensitive_ports]
     return res
-print(filter_sensitive_ports(read_csv_to_list("network_traffic.log")))
+# print(filter_sensitive_ports(read_csv_to_list("network_traffic.log")))
